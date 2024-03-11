68eb79b67dbf2f12f7d6ea54785bae4553153350
divyadiv917 <162984005+divyadiv917@users.noreply.github.com>
2024-03-11 16:15:16 +0530

    Add files via upload

---
+++ file4.py
@@ -0,0 +1,28 @@
+***WHAT KIND OF LANGUAGE IS PYTHON***
+python is a
+*Object oriented programing language
+*Interpreted Language
+*Dynamically typed language
+*Compiled language
+
+***FLAOVURS OF PYTHON***
+
+-&gt;cpython-written by c,most common implementation of python
+-&gt;jypython-Java implementation of python
+-&gt;ironpython-implementation of the python programminglanguage which is tightly integrated .NET
+-&gt;brpython-browser python runs in browser
+-&gt;rubypython-bridge between python and ruby interpreters
+-&gt;micropython-runs in micro controllers
+
+*********************
+#print has 5 parameters
+----&gt;print(object,sep,end,filr,flush)
+
+**OBJECT---&gt;the value to be printed is object
+#sep---&gt;separator
+#Eg
+#a=8
+#b=9
+#c=89
+#print(a,b,c,sep=&#39;$&#39;)
+#end--&gt;which is used to add the string at the end of the value
